3bbd7444d693a85fd8b667076dfa862673353a1a
Sarun Matipano <sarun.m@cdg.co.th>
2021-09-30 12:02:49 +0700

    add TDD_workshop

---
+++ src/main/java/day01/Hello.java
@@ -0,0 +1,8 @@
+package day01;
+
+public class Hello {
+
+    public String sayHello(String name){
+        return "day01.Hello "+name;
+    }
+}
---
+++ src/test/java/day01/HelloTest.java
@@ -0,0 +1,31 @@
+package day01;
+
+import day01.Hello;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HelloTest {
+
+    private  Hello hello;
+
+    @BeforeEach
+    public void initial(){
+        hello = new Hello();
+    }
+
+    @Test
+    @DisplayName("test sayHello 001")
+    public void case001(){
+        // arage
+        String expected = "day01.Hello Sarun.M";
+
+        // act
+       String actual = hello.sayHello("Sarun.M");
+
+       // aasert
+        assertEquals(expected, actual);
+    }
+}
